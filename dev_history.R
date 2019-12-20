c9fef63b6f35b93372281c1c400451d8053fb3cf
Antoine Bichat <a.bichat@yahoo.fr>
2019-12-20 16:49:50 +0100

    Dependencies

--- dev_history.R
+++ dev_history.R
@@ -8,10 +8,14 @@ library(usethis)
 
 badgecreatr::badge_projectstatus(status = "concept")
 
+attachment::att_to_description(extra.suggests =
+                                 c("devtools", "dplyr", "ggplot2",
+                                   "knitr", "bookdown", "thesisdown"))
 use_tidy_description()
 
 document()
 
+check()
 
 rmarkdown::render("README.Rmd")
 file.remove("README.html")
